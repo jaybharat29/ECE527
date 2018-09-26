f3fdf6133c84cbbe3aad19fbc97dcbb7a8ffe6cb
jaybharat29 <jaypatelstar@gmail.com>
2018-09-25 20:02:24 -0500

    part2 added

---
+++ exp/MP2/part2.1/part2.1.sdk/part2/Debug/src/helloworld.d
@@ -0,0 +1,58 @@
+src/helloworld.o src/helloworld.o: ../src/helloworld.c \
+ ../../part2_bsp/ps7_cortexa9_0/include/xaxidma.h \
+ ../../part2_bsp/ps7_cortexa9_0/include/xaxidma_bdring.h \
+ ../../part2_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../part2_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../part2_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../part2_bsp/ps7_cortexa9_0/include/xaxidma_bd.h \
+ ../../part2_bsp/ps7_cortexa9_0/include/xaxidma_hw.h \
+ ../../part2_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../part2_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../part2_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../part2_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../part2_bsp/ps7_cortexa9_0/include/bspconfig.h \
+ ../../part2_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../part2_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../part2_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../part2_bsp/ps7_cortexa9_0/include/xdebug.h \
+ ../../part2_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../part2_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../part2_bsp/ps7_cortexa9_0/include/xdebug.h
+
+../../part2_bsp/ps7_cortexa9_0/include/xaxidma.h:
+
+../../part2_bsp/ps7_cortexa9_0/include/xaxidma_bdring.h:
+
+../../part2_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../part2_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../part2_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../part2_bsp/ps7_cortexa9_0/include/xaxidma_bd.h:
+
+../../part2_bsp/ps7_cortexa9_0/include/xaxidma_hw.h:
+
+../../part2_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../part2_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../part2_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../part2_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../part2_bsp/ps7_cortexa9_0/include/bspconfig.h:
+
+../../part2_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../part2_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../part2_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../part2_bsp/ps7_cortexa9_0/include/xdebug.h:
+
+../../part2_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../part2_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../part2_bsp/ps7_cortexa9_0/include/xdebug.h:
---
+++ exp/MP2/part2.1/part2.1.sdk/part2/Debug/src/platform.d
@@ -0,0 +1,16 @@
+src/platform.o src/platform.o: ../src/platform.c \
+ ../../part2_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../part2_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../part2_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../part2_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../src/platform_config.h
+
+../../part2_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../part2_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../part2_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../part2_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../src/platform_config.h:
